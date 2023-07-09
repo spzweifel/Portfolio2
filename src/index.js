0a735743410a329a96fde527dce902e78f878fcf
Sean Zweifel <spzweifel@gmail.com>
2023-07-09 18:43:37 -0400

    got started, did some research, did minor coding

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// provides access to DOM-specific methods
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// renders React components to the DOM.
+ReactDOM.render(<App />, document.getElementById('root'));
