a56dc794a0368a1d5fabbbc86da58af31f62f122
Sean Zweifel <spzweifel@gmail.com>
2023-07-11 19:43:03 -0400

    contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,5 +4,93 @@
 // THEN I receive a notification that this field is required
 // WHEN I enter text into the email address field
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { validateEmail } from "../../Resources/util/helpers";
 
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const inputType = target.id;
+    const inputValue = target.inputValue;
+    const { target } = e;
+
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+      if (!validateEmail(inputValue)) {
+        setErrorMessage("Please, enter a valid email address.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      setMessage(inputValue);
+    }
+
+    //why is this not doing anything? reference line 89
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+  return (
+    <div className="main">
+      <form className="contactForm">
+        <h3>Contact Me</h3>
+        <p>
+          If you'd like to get into contact with me, please, leave your name and
+          a message.
+        </p>
+        <input
+          id="name"
+          className="input"
+          value={name}
+          placeholder="Name"
+          type="text"
+          onChange={handleInputChange}
+          onBlur={handleInputChange}
+        ></input>
+        <input
+          id="email"
+          className="input"
+          value={email}
+          placeholder="Email"
+          type="email"
+          onChange={handleInputChange}
+          onBlur={handleInputChange}
+        ></input>
+        <p>
+          {errorEmail && (
+            <span className="error-text text-danger">{errorMessage}</span>
+          )}
+        </p>
+        <textarea
+          id="message"
+          className="message"
+          value={message}
+          placeholder="Type your message here!"
+          type="text"
+          onChange={handleInputChange}
+          onBlur={handleInputChange}
+        ></textarea>
+        <input
+          id="submit"
+          type="submit"
+          className="submit"
+          value="Submit"
+          onClick={handleSubmit}
+        ></input>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
