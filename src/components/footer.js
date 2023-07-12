2de652f166832123761b6430c5b8772d3e3b7fba
Sean Zweifel <spzweifel@gmail.com>
2023-07-11 20:17:15 -0400

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = [
+  {
+    link: "https://github.com/spzweifel",
+  },
+  {
+    link: "https://www.linkedin.com/in/sean-zweifel-161768144/",
+  },
+  {
+    link: "https://www.instagram.com/sean.zweifel/"
+  }
+];
+
+export default Footer;
